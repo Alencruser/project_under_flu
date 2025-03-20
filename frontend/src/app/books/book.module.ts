33e61dbb23b60fa3571118e6775437d840230d4d
Alencruser <alencruserxx@hotmail.fr>
2025-03-20 15:35:32 +0100

    upgraded to v18

--- frontend/src/app/books/book.module.ts
+++ frontend/src/app/books/book.module.ts
@@ -5,21 +5,14 @@ import { BooksRoutingModule } from './books-routing.module';
 import { BookListComponent } from './components/book-list.component';
 import { BookFormComponent } from './components/book-form.component';
 import { BookService } from './services/book.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
-@NgModule({
-  declarations: [BookListComponent, BookFormComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BooksRoutingModule,
-    HttpClientModule,
-    MatButtonModule,
-    MatCardModule,
-  ],
-  providers: [BookService],
-})
+@NgModule({ declarations: [BookListComponent, BookFormComponent], imports: [CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BooksRoutingModule,
+        MatButtonModule,
+        MatCardModule], providers: [BookService, provideHttpClient(withInterceptorsFromDi())] })
 export class BooksModule {}
