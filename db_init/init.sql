c393d71b65576f170c3e4fb5c323e8125c6ac146
Alencruser <alencruserxx@hotmail.fr>
2025-04-19 17:28:59 +0200

    upgraded database to accept new features

--- db_init/init.sql
+++ db_init/init.sql
@@ -10,10 +10,37 @@ CREATE TABLE IF NOT EXISTS book (
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     note TEXT NULL,
-    rating INTEGER CHECK (note >= 0 AND note <= 5) NULL,
     cover TEXT NULL,
     published_date DATE NOT NULL,
     last_modification_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP,
+    created_by INTEGER,
+    CONSTRAINT fk_user FOREIGN KEY(created_by) REFERENCES "user"(id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS "role" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS book_rating (
+    book_id INTEGER,
     user_id INTEGER,
+    rating INTEGER,
+    CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES book(id) ON DELETE SET NULL,
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES "user"(id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS book_to_read (
+    book_id INTEGER,
+    user_id INTEGER,
+    CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES book(id) ON DELETE SET NULL,
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES "user"(id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS role_user (
+    role_id INTEGER,
+    user_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES "role"(id) ON DELETE SET NULL,
     CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES "user"(id) ON DELETE SET NULL
 );
